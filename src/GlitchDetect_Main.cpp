b0462b0ddce19c4fb6a308043556f07536b0345b
Helen Zhu <helen.zhu@karlstorz.com>
2017-08-10 14:41:12 -0400

    beta version, take camera ID input

--- src/GlitchDetect_Main.cpp
+++ src/GlitchDetect_Main.cpp
@@ -7,28 +7,46 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
 
+std::string ExePath()
+{
+	char buffer[MAX_PATH];
+	GetModuleFileName(NULL,buffer,MAX_PATH);
+	string::size_type pos=string(buffer).find_last_of("\\/");
+	return string(buffer).substr(0,pos);
+}
+
 int main(int, char**)
 
 {
-	cout<<"enter camera ID"<<endl;
+	cout<<"enter camera ID (0,1,...)"<<endl;
 	int cameraID;
 	cin>>cameraID;
 
     VideoCapture cap(cameraID); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
+    if(!cap.isOpened()) //check if camera is open
+    {
+    	cout<<"camera is not available. Press Enter key to end program."<<endl;
+        std::getchar();
+        std::getchar();
+        waitKey(0);
+    	return 0;
+    }
+
     namedWindow("preview",1);
-    cout<<"press any key when ready to capture test pattern"<<endl;
+    cout<<"press Space key when ready to capture test pattern"<<endl;
+
     for(;;)
     {
     	Mat preview;
     	cap>>preview;
     	imshow("preview",preview);
-    	if(waitKey(15) >= 0) break;
+    	if(waitKey(15) ==32) break;
+    	//if(waitKey(15) ==27) exit(0);
     }
 
     Mat diff,testPattern;
@@ -36,6 +54,8 @@ int main(int, char**)
    // namedWindow("video",1);
     //namedWindow("testPattern",1);
    // namedWindow("diff",1);
+    std::string glitchDataFolder = ExePath()+ "\\GlitchScreenShot\\";
+    CreateDirectory(glitchDataFolder.c_str(),NULL);
 
     int i=0;
     for(;;)
@@ -63,7 +83,7 @@ int main(int, char**)
                         std::string imageName;
                         ostringstream convert;
                         convert<<i;
-						imageName=convert.str()+ ".bmp";
+						imageName=glitchDataFolder+convert.str()+ ".bmp";
         	        	cout<<imageName<<endl;
         	        	imwrite(imageName,frame);
         	        }else{
@@ -81,6 +101,6 @@ int main(int, char**)
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
 
-    waitKey(0);
+
     return 0;
 }
